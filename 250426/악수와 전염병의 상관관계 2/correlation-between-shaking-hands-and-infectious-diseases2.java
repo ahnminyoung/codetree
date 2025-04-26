3fde5a7c9bc6b24cfceb08737540309132a9768b
ahnminyoung <anrh0213@nate.com>
2025-04-26 22:20:50 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 137ms, Memory: 10MB, Status: Passed - Codetree

--- 250426/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
+++ 250426/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
@@ -1,23 +1,26 @@
 import java.util.*;
 public class Main {
-    static class Meeting implements Comparable<Meeting> {
+    static class Shake implements Comparable<Shake> {
         int time;
-        int x;
-        int y;
-
-        Meeting(int time, int x, int y) {
+        int person1;
+        int person2;
+        
+        public Shake(int time, int person1, int person2) {
             this.time = time;
-            this.x = x;
-            this.y = y;
+            this.person1 = person1;
+            this.person2 = person2;
         }
 
         @Override
-        public int compareTo(Meeting other) {
-            return Integer.compare(this.time, other.time);
+        public int compareTo(Shake shake) {
+            // 시간을 기준으로 오름차순으로 정렬합니다.
+            return time - shake.time;
         }
-    }
+    };
 
     public static void main(String[] args) {
+        int MAX_T = 250;
+        int MAX_N = 100;
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
@@ -25,43 +28,41 @@ public class Main {
         int P = sc.nextInt();
         int T = sc.nextInt();
 
+        int[] shakeNum = new int[N];
         int[] infected = new int[N];
-        int[] canInfect = new int[N];
 
         infected[P - 1] = 1;
-        canInfect[P - 1] = K;
 
-        List<Meeting> meetings = new ArrayList<>();
+        List<Shake> shakes = new ArrayList<>();
 
         for (int i = 0; i < T; i++) {
             int t = sc.nextInt();
             int x = sc.nextInt() - 1;
             int y = sc.nextInt() - 1;
-            meetings.add(new Meeting(t, x, y));
+            shakes.add(new Shake(t, x, y));
         }
 
-        Collections.sort(meetings);
+        Collections.sort(shakes);
 
-        for (Meeting meeting : meetings) {
-            int x = meeting.x;
-            int y = meeting.y;
+        for (Shake shake : shakes) {
+            int x = shake.person1;
+            int y = shake.person2;
 
             // 감염 전파: 감염된 사람 -> 비감염자에게만
-            if (infected[x] == 1 && canInfect[x] > 0 && infected[y] == 0) {
-                infected[y] = 1;
-                canInfect[y] = K;
+           if (infected[x] == 1) {
+                shakeNum[x]++;
             }
-            if (infected[y] == 1 && canInfect[y] > 0 && infected[x] == 0) {
-                infected[x] = 1;
-                canInfect[x] = K;
+            if (infected[y] == 1) {
+                shakeNum[y]++;
             }
 
+
             // 악수한 감염자들의 전염 가능 횟수 감소
-            if (infected[x] == 1 && canInfect[x] > 0) {
-                canInfect[x]--;
+            if (infected[x] == 1 && shakeNum[x] <= K) {
+                infected[y] = 1;
             }
-            if (infected[y] == 1 && canInfect[y] > 0) {
-                canInfect[y]--;
+            if (infected[y] == 1 && shakeNum[y] <= K) {
+                infected[x] = 1;
             }
         }
 
